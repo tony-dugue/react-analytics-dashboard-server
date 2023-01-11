e35cfa6ee30f6dcff50d4b06ed8167b4294e1a19
tony dugué <duguetony@gmail.com>
2023-01-11 21:03:45 +0100

    Added Mongoose UserSchema & request getUser + fake user data in MongoDB

--- routes/client.js
+++ routes/client.js
@@ -1,4 +1,4 @@
-import express, { Router } from "express";
+import express from "express";
 
 const router = express.Router();
 
--- routes/general.js
+++ routes/general.js
@@ -1,6 +1,8 @@
-import express, { Router } from "express";
+import express from "express";
+import { getUser } from "../controllers/general.js";
 
 const router = express.Router();
 
-export default router;
+router.get("/user/:id", getUser);
 
+export default router;
