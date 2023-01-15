95ce135ef5c7b4285308fb69b2cb005850f95d0d
tony dugué <duguetony@gmail.com>
2023-01-15 23:15:13 +0100

    Added Transaction route & GET request with search options for pagination

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ import salesRoutes from "./routes/sales.js";
 import User from "./models/User.js";
 import Product from "./models/Product.js";
 import ProductStat from "./models/ProductStat.js";
-import { dataUser, dataProduct, dataProductStat } from "./data/index.js";
+import Transaction from "./models/Transaction.js";
+import { dataUser, dataProduct, dataProductStat, dataTransaction } from "./data/index.js";
 
 /* CONFIGURATION */
 dotenv.config();
@@ -48,7 +49,7 @@ mongoose
     /* ONLY ADD DATA ONE TIME */
     //Product.insertMany(dataProduct);
     //ProductStat.insertMany(dataProductStat);
+    //Transaction.insertMany(dataTransaction);
     //User.insertMany(dataUser);
-    
   })
   .catch((error) => console.log(`${error} did not connect`));
