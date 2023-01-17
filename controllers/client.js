92bd6215fafd2a675d322650a4e7d3fc36c7f0e5
tony dugué <duguetony@gmail.com>
2023-01-17 18:54:02 +0100

    Added geography route & GET request

--- controllers/client.js
+++ controllers/client.js
@@ -2,6 +2,7 @@ import Product from "../models/Product.js";
 import ProductStat from "../models/ProductStat.js";
 import User from "../models/User.js";
 import Transaction from "../models/Transaction.js";
+import getCountryIso3 from "country-iso-2-to-3";
 
 export const getProducts = async (req, res) => {
   try {
@@ -29,7 +30,7 @@ export const getCustomers = async (req, res) => {
   }
 };
 
-export const getTransaction = async (req, res) => {
+export const getTransactions = async (req, res) => {
   try {
     // sort should look like this: { "field": "userId", "sort": "desc"}
     const { page = 1, pageSize = 20, sort = null, search = "" } = req.query;
@@ -37,7 +38,9 @@ export const getTransaction = async (req, res) => {
     // formatted sort should look like { userId: -1 }
     const generateSort = () => {
       const sortParsed = JSON.parse(sort);
-      const sortFormatted = { [sortParsed.field]: (sortParsed.sort = "asc" ? 1 : -1) };
+      const sortFormatted = {
+        [sortParsed.field]: (sortParsed.sort = "asc" ? 1 : -1),
+      };
       return sortFormatted;
     };
 
@@ -58,8 +61,28 @@ export const getTransaction = async (req, res) => {
     });
 
     res.status(200).json({ transactions, total });
-
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
 };
+
+export const getGeography = async (req, res) => {
+  try {
+    const users = await User.find();
+
+    const mappedLocations = users.reduce( (acc, { country }) => {
+      const countryISO3 = getCountryIso3(country);
+      if (!acc[countryISO3]) acc[countryISO3] = 0;
+      acc[countryISO3]++;
+      return acc;
+    }, {})
+
+    const formattedLocations = Object.entries(mappedLocations).map( ([country, count]) => {
+      return { id: country, value: count }
+    })
+
+    res.status(200).json(formattedLocations);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+}
