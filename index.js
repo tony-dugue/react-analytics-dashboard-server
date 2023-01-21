95b19ab700755b688e1f4bfb5e1a63cd75026c0f
tony dugué <duguetony@gmail.com>
2023-01-21 12:53:38 +0100

    Added sales route & GET request for overallStats

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ import User from "./models/User.js";
 import Product from "./models/Product.js";
 import ProductStat from "./models/ProductStat.js";
 import Transaction from "./models/Transaction.js";
-import { dataUser, dataProduct, dataProductStat, dataTransaction } from "./data/index.js";
+import OverallStat from "./models/OverallStat.js";
+import { dataUser, dataProduct, dataProductStat, dataTransaction, dataOverallStat } from "./data/index.js";
 
 /* CONFIGURATION */
 dotenv.config();
@@ -50,6 +51,7 @@ mongoose
     //Product.insertMany(dataProduct);
     //ProductStat.insertMany(dataProductStat);
     //Transaction.insertMany(dataTransaction);
+    //OverallStat.insertMany(dataOverallStat);
     //User.insertMany(dataUser);
   })
   .catch((error) => console.log(`${error} did not connect`));
